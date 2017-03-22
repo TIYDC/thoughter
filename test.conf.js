39728a6599faed33a47ebdb0b7cda14dfb89f783
Brian Fiala <bgfiala@gmail.com>
2017-03-21 22:06:06 -0400

    played with conf files to try to get DOM additions through karma to work. Still not working

---
+++ test.conf.js
@@ -0,0 +1,13 @@
+module.exports = function karmaConfig( config) {
+  config.set({
+    frameworks: ['mocha', 'chai'],
+    browsers: ['Chrome'],
+    singleRun: true,
+    files: [
+      // globbing pattern for searching src directory and any sub for any js
+      'src/**/*.js',
+      'node_modules/fetch-mock/es5/client-browserified.js',
+      'test/specs/**/*.js'
+    ]
+  });
+};
