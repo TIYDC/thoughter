51b5fc6e7def97682ca0a7bef3734bc9883da668
Steven Strasburg <SSStrasburg@gmail.com>
2017-04-03 23:35:30 -0400

    adding gruntfile from branch to master

---
+++ review/review.js
@@ -0,0 +1,22 @@
+// readme
+// clone the repository
+// npm install
+//
+// how to run test;
+// grunt karma
+//
+// how to build final product:
+//grunt build
+//
+
+//index.html
+//link href matches the build directory
+
+//gruntfile.js
+//clean: ...
+//copy: ...
+//**/*.js  //find all files **,  *.js all that end with js
+//
+//watch:  // watching for individual changes
+//gruntConfig.registerTask('gruntWatch',['watch:watchGrunt'])
+//or from terminal: grunt watch
